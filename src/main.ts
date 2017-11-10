91024e233fe638f10a5ebc988e8f74c18d141f50
mtpya <19mtp84@gmail.com>
2017-11-10 14:51:12 +0200

    Revert "test changes"
    This reverts commit f3c4320913561bf79220eddab3d2a062fc014483.

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,6 @@ let result;
 result = recursion(110);
 resultEl.innerHTML = `<h2>Recursion result: ${result}</h2>`;
 
-let value: number = 123;
-let value2: number;
-
-function recursion(n: number) {
+function recursion(n) {
   return n === 1 ? 1 : n * recursion(n - 1);
 }
-
-value2 = value * 2;
