f3c4320913561bf79220eddab3d2a062fc014483
mtpya <19mtp84@gmail.com>
2017-11-10 13:15:46 +0200

    test changes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ let result;
 result = recursion(110);
 resultEl.innerHTML = `<h2>Recursion result: ${result}</h2>`;
 
-function recursion(n) {
+let value: number = 123;
+let value2: number;
+
+function recursion(n: number) {
   return n === 1 ? 1 : n * recursion(n - 1);
 }
+
+value2 = value * 2;
